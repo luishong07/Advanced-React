9136eda42e89393546b8ee5d0edf38b84e20250c
Luis Hong <luishong50@gmail.com>
2021-03-19 14:22:18 -0500

    number 17 done

--- sick-fits/backend/schemas/ProductImage.ts
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { text } from "@keystone-next/fields";
+import { relationship, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
 import { cloudinaryImage } from "@keystone-next/cloudinary"
 
@@ -15,7 +15,8 @@ export const ProductImage = list({
             cloudinary,
             label:'Source'
         }),
-        altText: text()
+        altText: text(),
+        product: relationship({ref: 'Product.photo'})
 
     }
 })
