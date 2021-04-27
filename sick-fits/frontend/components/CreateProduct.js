8b1f3d7d772213fa82a41d498306601091de9231
Luis Hong <luishong50@gmail.com>
2021-04-27 15:18:05 -0500

    number 24 25 26 done

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,5 +1,38 @@
+import { useMutation } from "@apollo/client"
 import useForm from "../lib/useForm"
 import Form from "./styles/Form"
+import gql from "graphql-tag"
+import DisplayError from "./ErrorMessage"
+import {ALL_PRODUCTS_QUERY} from './Products'
+import Router from 'next/router'
+
+const CREATE_PRODUCT_MUTATION = gql`
+    mutation CREATE_PRODUCT_MUTATION(
+        # which variables are getting passed in and what type are they?
+        $name: String!
+        $description: String!
+        $price: Int!
+        $image: Upload 
+    ){
+        createProduct(data:{
+            name: $name,
+            description: $description,
+            price: $price,
+            status: "AVAILABLE"
+            photo: {
+                create: {
+                    image: $image
+                    altText: $name
+                }
+            }
+        }){
+            id 
+            description
+            price
+            name
+        }
+    }
+`
 
 export default function CreateProduct(){
     const {inputs, handleChange, clearForm, resetForm} = useForm({
@@ -8,12 +41,29 @@ export default function CreateProduct(){
         price: 1000,
         description: "these are the best shoes"
     })
+
+    const [createProduct, {loading, error, data}] = useMutation(
+        CREATE_PRODUCT_MUTATION, 
+        {
+            variables: inputs,
+            refetchQueries: [{query: ALL_PRODUCTS_QUERY}]
+        }
+    )
+    // console.log(createProduct)
+
     return(
-        <Form onSubmit = {(e) => {
+        <Form onSubmit = { async (e) => {
             e.preventDefault()
-            console.log(inputs)
+            //submit inputs fiels to backend
+            const res = await createProduct()
+            clearForm()
+            //go to that product page
+            Router.push({
+                pathname: `/product/${res.data.createProduct.id}`
+            })
         }}>
-            <fieldset>
+            <DisplayError error = {error}/>
+            <fieldset disabled = {loading} aria-busy = {loading}>
             <label htmlFor = "image">
                     Image
                     <input 
--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -1,48 +1,46 @@
-import { validate } from "graphql"
-import { number } from "prop-types"
-import { useState } from "react"
+import { useState } from 'react';
 
-export default function useForm(initial = {}){
-//create state object for our inputs 
-    const [inputs, setInputs] = useState(initial)
-    
-    // {
-        //     name: "shoes",
-        //     description: "nice shoes",
-        //     price: 1000
-        // }
-        
-    function handleChange(e){
-        let {value, name, type} = e.target
-        if(type === 'number'){
-            value = parseInt(value)
-        }
-        if(type === 'files'){
-            [value] = e.target.files
-        }
-        //copy existing state
-        setInputs({
-            ...inputs,
-            [name] : value
-        }) 
-    }
+export default function useForm(initial = {}) {
+  // create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
 
-    function resetForm(){
-        setInputs(initial)
-    }
+  // {
+  //   name: 'wes',
+  //   description: 'nice shoes',
+  //   price: 1000
+  // }
 
-    function clearForm(){
-        const blankSlate = Object.fromEntries(Object.entries(inputs).map(([key,value]) => [key, ''])) 
-        setInputs(blankSlate)
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+    if (type === 'number') {
+      value = parseInt(value);
     }
-    
+    if (type === 'file') {
+      [value] = e.target.files;
+    }
+    setInputs({
+      // copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
 
-    //return things we want to surface from this custome hook
-    // clearForm()
-    return {
-        inputs,
-        handleChange,
-        resetForm,
-        clearForm
-    }
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  // return the things we want to surface from this custom hook
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
 }
